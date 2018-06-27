db40d13812e8c54dd52c213c55795334b9f1913a
Andrey Savchenko <contact@rarst.net>
2018-06-27 20:18:42 +0300

    Corrected white space in tests

--- tests/phpunit/tests/date/dateI18n.php
+++ tests/phpunit/tests/date/dateI18n.php
@@ -69,14 +69,14 @@ class Tests_Date_I18n extends WP_UnitTestCase {
 	}
 
 	public function test_short_c_format(){
-        update_option( 'timezone_string', 'America/Regina' );
+		update_option( 'timezone_string', 'America/Regina' );
 
 		$this->assertEquals( strtotime( date( DATE_W3C ) ), strtotime( date_i18n( 'c' ) ), 'The dates should be equal', 2 );
 		$this->assertEquals( 'c', date_i18n( '\c' ) );
 	}
 
 	public function test_short_r_format(){
-        update_option( 'timezone_string', 'America/Regina' );
+		update_option( 'timezone_string', 'America/Regina' );
 
 		$this->assertEquals( strtotime( date( DATE_RFC2822 ) ), strtotime( date_i18n( 'r' ) ), 'The dates should be equal', 2 );
 		$this->assertEquals( 'r', date_i18n( '\r' ) );
